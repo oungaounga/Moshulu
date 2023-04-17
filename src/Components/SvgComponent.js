8c271372f8d9f5ebf6a9ee52b095bd452ea32f33
razal <oungaounga@gmail.com>
2023-04-17 11:46:05 +0200

    used a web safe font for the svg

--- src/Components/SvgComponent.js
+++ src/Components/SvgComponent.js
@@ -30,8 +30,6 @@ function SvgComponent(props) {
     <text
       xmlns="http://www.w3.org/2000/svg"
       style={svgTextStyle}
-      // font-family="courier"
-      font-family="monospace"
       x="20"
       y={index * 6.5}
       key={index}
