5c329d1d1af140eea1af816815ff4c8da3083d8f
razal <oungaounga@gmail.com>
2023-04-18 14:10:17 +0200

    rebuild

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      body: ['"Open Sans"', 'monospace'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
